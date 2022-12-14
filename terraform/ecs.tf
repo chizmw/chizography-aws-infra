7ea1f5eca947f4ff14e4ccaef9c48d8cbe21ddc4
Chisel <chisel@malik-wright.uk>
2022-12-14 14:26:22 +0000

    add nginx service

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,4 +1,34 @@
 # Create the ECS cluster
-resource "aws_ecs_cluster" "ecs_cluster" {
+resource "aws_ecs_cluster" "ecs_cluster_websites" {
+  # checkov:skip=CKV_AWS_65: ADD REASON
   name = "websites"
+}
+
+
+# Create nginx reverse proxy service in ECS cluster
+resource "aws_ecs_service" "nginx" {
+  name            = "nginx-service"
+  cluster         = aws_ecs_cluster.ecs_cluster_websites.id
+  task_definition = aws_ecs_task_definition.nginx.arn
+  desired_count   = 1
+}
+
+# Create ECS task definition for nginx reverse proxy
+resource "aws_ecs_task_definition" "nginx" {
+  family                = "nginx"
+  container_definitions = <<DEFINITION
+[
+  {
+    "name": "nginx",
+    "image": "nginx:latest",
+    "portMappings": [
+      {
+        "containerPort": 80,
+        "hostPort": 80
+      }
+    ],
+    "essential": true
+  }
+]
+DEFINITION
 }
